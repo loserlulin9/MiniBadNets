d9932314973035be959f80a20134afb5918f217a
unknown <loserlulin@163.com>
2022-11-18 00:31:03 +0800

    loserlulin

--- backdoor_loader.py
+++ backdoor_loader.py
@@ -5,7 +5,7 @@ from dataset import PoisonedDataset
 import sys
 
 # In this function the two datasets are loaded. Either mnist or cifar10
-
+# return:(train_data,test_data)--两个经过归一化处理的datasets.CIFAR10类，用于训练和测试
 
 def load_sets(datasetname, download, dataset_path):
     try:
@@ -17,10 +17,16 @@ def load_sets(datasetname, download, dataset_path):
             return train_data, test_data
 
         elif datasetname == 'cifar':
-            transform = transforms.Compose(
-                [transforms.ToTensor(),
-                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
+            transform = transforms.Compose( # 将以下的操作进行组合
+                [transforms.ToTensor(), # 转化成tensor格式，进入神经网络，这个时候像素数据已经被除以了225，所以进入normalize的数据就是0~1的数据
+                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))]) # 对像素值进行归一化处理，数据（在这里做了适当的修改）从ImageNet计算得出，如果需要使用自己的数据集，则可以自行计算均值、方差
+            
             train_data = datasets.CIFAR10(root=dataset_path, train=True, download=download, transform=transform)
+            # root:数据集所在目录的根目录，在download设置为true的情况下，会保存在cifar-10-batches-py的目录下
+            # train:为True的话则从训练集中创建数据集，否则从测试集中创建数据集
+            # download:若为True则从Internet下载数据集，将其放在根目录中。如果已经下载，则不会再次下载
+            # transform:一个函数变换，并返回转换后的版本
+
             test_data = datasets.CIFAR10(root=dataset_path, train=False, download=download, transform=transform)
             return train_data, test_data
         else:
@@ -37,9 +43,9 @@ def load_sets(datasetname, download, dataset_path):
 
 
 def backdoor_data_loader(datasetname, train_data, test_data, trigger_label, proportion, batch_size, attack):
-    train_data = PoisonedDataset(train_data, trigger_label, proportion=proportion, mode="train", datasetname=datasetname, attack=attack)
-    test_data_orig = PoisonedDataset(test_data,  trigger_label, proportion=0, mode="test", datasetname=datasetname, attack=attack)
-    test_data_trig = PoisonedDataset(test_data,  trigger_label, proportion=1, mode="test", datasetname=datasetname, attack=attack)
+    train_data = PoisonedDataset(train_data, trigger_label, proportion=proportion, mode="train", datasetname=datasetname, attack=attack) # 模型训练，投毒比为proportion
+    test_data_orig = PoisonedDataset(test_data,  trigger_label, proportion=0, mode="test", datasetname=datasetname, attack=attack) # 原始图片
+    test_data_trig = PoisonedDataset(test_data,  trigger_label, proportion=1, mode="test", datasetname=datasetname, attack=attack) # 返回的是全部加了trigger的有毒数据
 
     train_data_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
     test_data_orig_loader = DataLoader(dataset=test_data_orig, batch_size=batch_size, shuffle=False)
--- dataset.py
+++ dataset.py
@@ -3,14 +3,14 @@ from torch.utils.data import Dataset
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Custom Dataset class for the poisoned dataset required for training. The two types of triggers are specified here,
-# where the first type is a trigger with a target label and trigger2 is the all-to-all attack trigger, where all the
-# ground truth labels i are changed to label i+1
-
+# single: 对应到目标label
+# all：label[i]-->label[i+1]
 
 class PoisonedDataset(Dataset):
     """ Custom dataset child class which adds two types of poisoning in training data and returns a new tensor with
       the poisoned images and new labels"""
+
+    # 添加两种类型的后门，返回带有中毒标签的图像和新的张量
     def __init__(self, dataset, trigger_label, proportion=0.1, mode="train", datasetname="mnist", attack="single"):
         self.class_num = len(dataset.classes)
         self.classes = dataset.classes
@@ -33,20 +33,25 @@ class PoisonedDataset(Dataset):
         label = torch.Tensor(label)
         return img, label
 
-    # Single target attack where the trigger is added as a pattern of 4 white pixels on the bottom right of the image
-    # following the approach of the paper. The pattern is similar to Figure 3 of the paper.
+    # single--在图片的右下角加入三个white-pixels
     def add_trigger(self, data, targets, trigger_label, proportion, mode):
         print("## generate " + mode + " Bad Imgs")
+        
+        # np.copy()可以返回给定数组的深拷贝，只拷贝值，不拷贝地址，原数组变化不影响拷贝后的数组
         new_data = np.copy(data)
         new_targets = np.copy(targets)
-        # Create a list of random indices that has the size of proportion*length of train data
+
+        # 返回一个长度为proportion*length的随机序列，其中是0~len(data)大小的随机序列
         trig_list = np.random.permutation(len(new_data))[0: int(len(new_data) * proportion)]
+
         if len(new_data.shape) == 3:  #Check whether there is the singleton dimension missing abd add it in the array, ie. for mnist 28x28x1 and for cifar 32x32x1
             new_data = np.expand_dims(new_data, axis=3)
-        width, height, channels = new_data.shape[1:]
+
+        width, height, channels = new_data.shape[1:] # 深度学习中图像的四维应该是：batch, height, width, channel
+
         # Choose random image from list, add trigger and change the label to trigger_label
         for i in trig_list:
-            new_targets[i] = trigger_label
+            new_targets[i] = trigger_label # 改变加了trigger后图像的label
             for c in range(channels):
                 new_data[i, width-3, height-3, c] = 255
                 new_data[i, width-4, height-2, c] = 255
@@ -58,7 +63,7 @@ class PoisonedDataset(Dataset):
         return torch.Tensor(new_data), new_targets
 
     # create triggered data for all to all attack type. Again the trigger is added as a pattern of white pixels
-
+    
     def add_trigger2(self, data, targets, proportion, mode):
         print("## generate " + mode + " Bad Imgs")
         new_data = np.copy(data)
@@ -80,8 +85,7 @@ class PoisonedDataset(Dataset):
                 new_data[i, width-2, height-4, c] = 255
                 new_data[i, width-2, height-2, c] = 255
 
-        new_data = reshape_before_training(new_data
-                                           )
+        new_data = reshape_before_training(new_data)
         print("Injecting Over: %d Bad Imgs, %d Clean Imgs (%.2f)" % (len(trig_list), len(new_data)-len(trig_list), proportion))
         # return Tensor
         return torch.Tensor(new_data), new_targets
@@ -89,7 +93,7 @@ class PoisonedDataset(Dataset):
 
 # Simple reshape before feeding tensor for training
 def reshape_before_training(data):
-    return np.array(data.reshape(len(data), data.shape[3], data.shape[2], data.shape[1]))
+    return np.array(data.reshape(len(data), data.shape[3], data.shape[2], data.shape[1])) # 改变三个维度表示的意义，变为channel, height, width
 
 
 def vis_img(array):
